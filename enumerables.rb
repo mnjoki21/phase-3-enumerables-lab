4a7a45caf95e324945044ce0f0cbca53dc711ffa
mnjoki21 <mitchellenjoki7@gmail.com>
2022-08-18 20:27:38 +0300

    done

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,19 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+  spicy_foods.map do |obj|
+    obj[:name]
+    
+  end
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+  spicy_foods.filter do |obj|
+    obj[:heat_level] > 5
+  end
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -28,17 +35,26 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+  spicy_foods.map do |obj|
+    puts "#{obj[:name]} (#{obj[:cuisine]}) | Heat Level: #{'🌶'*obj[:heat_level]}"
+  end
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
+  spicy_foods.filter do |obj|
+    return obj if obj [:cuisine]== cuisine
+  end
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
+  spicy_foods,sort_by do |food|
+    food [:heat_level]
+  end
   # your code here
 end
 
@@ -48,10 +64,16 @@ end
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
   # your code here
+  spiciest = spicy_foods.filter {|food| [:heat_level] > 5}
+  spiciest.each do |food|
+    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{'🌶'*food[:heat_level]}"
+  end
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
+  total_heat_levels = spicy_foods.sum {|food| food[:heat_level]}
+  total_heat_levels/spicy_food.size
   # your code here
 end
